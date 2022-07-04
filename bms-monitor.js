886c601383a03b3e56a9d96565d9dee4c28a552b
Chaitra74 <chai7495@gmail.com>
2022-07-04 09:13:38 +0530

    Updated based on code review

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,28 +1,82 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const Battery = require('./battery');
-const BMS_FIXED = require('./bmsConfig');
+const { BMS_FIXED, GLOBAL_LANGUAGE } = require('./bmsConfig');
+const translate = require('@vitalets/google-translate-api');
 
+checkBatteryState = function (value, ranges) {
+	isValidRange = false;
+	ranges.forEach((range) => {
+		if (value >= range.min && value < range.max) {
+			isValidRange = range.isRangeValid;
+			printMessage(range.message);
+		}
+	});
+	return isValidRange;
+};
 
-isPropertyOutOfRange = function(name,battery, bmsFixedValue) { 
-    let isOutOfRange = false;
-    if (battery[name] > bmsFixedValue.max || battery[name] < bmsFixedValue.min) {
-        console.log(`${bmsFixedValue.name} is out of range! - ${battery.printAllBatteryValues()}`);
-        isOutOfRange = true;
-    }
-    return isOutOfRange;
-}
+printMessage = function (message) {
+	if (message) {
+		translate(message, {
+			to: GLOBAL_LANGUAGE,
+		}).then((res) => console.log(res.text));
+	}
+};
 
 function batteryIsOk(battery, bmsFixedValues) {
-   return !(isPropertyOutOfRange("temperature",battery,bmsFixedValues.temperature) || 
-             isPropertyOutOfRange("stateOfCharge",battery,bmsFixedValues.stateOfCharge)|| 
-             isPropertyOutOfRange("chargeRate",battery,bmsFixedValues.chargeRate))       
+	let tempState = checkBatteryState(
+		battery['temperature'],
+		getRanges(bmsFixedValues.temperature)
+	);
+	let socState = checkBatteryState(
+		battery['stateOfCharge'],
+		getRanges(bmsFixedValues.stateOfCharge)
+	);
+	let crState = checkBatteryState(
+		battery['chargeRate'],
+		getRanges(bmsFixedValues.chargeRate)
+	);
+	return tempState && socState && crState;
 }
 
-expect(batteryIsOk(new Battery(25, 70, 0.7), BMS_FIXED)).to.be.true;
+function getRanges(params) {
+	let ranges = [
+		{
+			min: -Infinity,
+			max: params.min,
+			message: `${params.name} out of range. Low ${params.name} breach`,
+			isRangeValid: false,
+		},
+		{
+			min: params.min + params.warningPoint,
+			max: params.max - params.warningPoint,
+			isRangeValid: true,
+		},
+		{
+			min: params.max,
+			max: Infinity,
+			message: `${params.name} out of range. High ${params.name} breach`,
+			isRangeValid: false,
+		},
+		{
+			min: params.min,
+			max: params.min + params.warningPoint,
+			message: `Warning: ${params.name} Approaching low`,
+			isRangeValid: true,
+		},
+		{
+			min: params.max - params.warningPoint,
+			max: params.max,
+			message: `Warning: ${params.name} Approaching peak`,
+			isRangeValid: true,
+		},
+	];
+	return ranges;
+}
+
+expect(batteryIsOk(new Battery(45, 77, 0.9), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(50, 70, 0.8), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(40, 90, 0.8), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(20, 70, 0.9), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(-25, 70, 0.8), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(40, 10, 0.8), BMS_FIXED)).to.be.false;
 expect(batteryIsOk(new Battery(20, 100, 0.08), BMS_FIXED)).to.be.false;
-
--- bmsConfig.js
+++ bmsConfig.js
@@ -1,17 +1,25 @@
 const BMS_FIXED = {
-    temperature: {      
-      min: 0,
-      max: 45,
-      name: "Temperature"
-    },
-    stateOfCharge: {
-      min: 20,
-      max: 80,   
-      name: "State of charge"  
-    },
-    chargeRate: {     
-      max: 0.8, 
-      name: "Charge rate"    
-    },
-  };
-  module.exports = BMS_FIXED;
+	temperature: {
+		min: 0,
+		max: 45,
+		name: 'Temperature',
+		keyName: 'temperature',
+		warningPoint: 2.25,
+	},
+	stateOfCharge: {
+		min: 20,
+		max: 80,
+		name: 'State of charge',
+		keyName: 'stateOfCharge',
+		warningPoint: 4,
+	},
+	chargeRate: {
+		max: 0.8,
+		name: 'Charge rate',
+		keyName: 'chargeRate',
+		warningPoint: 0.04,
+	},
+};
+
+const GLOBAL_LANGUAGE = 'de';
+module.exports = { BMS_FIXED, GLOBAL_LANGUAGE };
